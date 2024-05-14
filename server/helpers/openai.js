aadb4d8248a3c42eb78af8699e07641755617ba7
dwriz <dwikyrizkya27@gmail.com>
2024-05-15 00:34:43 +0700

    server | chore: add createImageUrl() to helper openai.js

--- server/helpers/openai.js
+++ server/helpers/openai.js
@@ -1,17 +1,16 @@
 const { OpenAI } = require("openai");
-// require("dotenv").config()
 
-async function openAI() {
-  const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+async function createImagePrompts() {
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
         content:
-          "You are a helpful assistant. I need an array of strings containing 4 items. First and second item is a subject. It could be anything but if it's a person, make it a guessable profession like police, soldier, nurse, etc. or a very famous person. Anything else could be an animal or something else. Third item should be an activity. Make it specific, like 'cooking fried rice' or 'playing chess'. Fourth item will be the fusion of first, second, and third item (if first item is 'soldier', second item is 'cat', third item is 'cooking fried rice' the result will be 'soldier and cat cooking fried rice'. This will be used as a prompt to generate image to DALL-E model for an image-guessing game, so make the combination as funny and as random as possible. Do not give me another answer other than an array and requested items inside that array.",
+          "You are a helpful assistant. I need an array of strings containing 4 items. First and second item is a subject. It could be anything but if it's a person, make it a guessable profession like police, soldier, nurse, etc. or a very famous person. Anything else could be an animal or something else. Make it creative. If the first item is a person, sometimes make the second item an animal. Just be creative, and don't be repetitive. Third item should be an activity. Make it specific, like 'cooking fried rice' or 'playing chess'. Fourth item will be the fusion of first, second, and third item (if first item is 'soldier', second item is 'cat', third item is 'cooking fried rice' the result will be 'soldier and cat cooking fried rice'. This will be used as a prompt to generate image to DALL-E model for an image-guessing game, so make the combination as funny and as random as possible. Do not give me another answer other than an array and requested items inside that array.",
       },
     ],
     model: "gpt-3.5-turbo",
@@ -20,4 +19,16 @@ async function openAI() {
   return completion.choices[0].message.content;
 }
 
-module.exports = { openAI };
+async function createImageUrl(prompt) {
+  const response = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: prompt,
+    n: 1,
+    size: "1024x1024",
+    style: "vivid",
+  });
+
+  return response.data[0].url;
+}
+
+module.exports = { createImagePrompts, createImageUrl };
