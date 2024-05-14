64c7f6daef157ffb72230e4d4c424361975dc601
dwriz <dwikyrizkya27@gmail.com>
2024-05-14 21:49:28 +0700

    server | feat: finish login() in UserController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,25 @@
+const { User } = require("../models/index.js");
+const { comparePassword } = require("../helpers/bcrypt.js");
+const { createToken } = require("../helpers/jwt.js");
+
 class UserController {
-  static async login(req, res) {
+  static async login(req, res, next) {
     try {
-      res.send("This is /login route");
+      const { email, password } = req.body;
+
+      if (!email) throw { name: "EmailEmpty" };
+      if (!password) throw { name: "PassEmpty" };
+
+      const user = await User.findOne({ where: { email } });
+      if (!user) throw { name: "InvalidLogin" };
+
+      const checkUser = comparePassword(password, user.password);
+      if (!checkUser) throw { name: "InvalidLogin" };
+
+      const token = createToken({ id: user.id });
+      res.status(200).json({ token });
     } catch (error) {
-      res.send(error);
+      next(error);
     }
   }
 
@@ -11,7 +27,7 @@ class UserController {
     try {
       res.send("This is /register route");
     } catch (error) {
-      res.send(error);
+      next(error);
     }
   }
 }
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { UserController } = require("../controllers/userController.js");
 
-router.get("/login", UserController.login);
+router.post("/login", UserController.login);
 router.get("/register", UserController.register);
 
 module.exports = router;
