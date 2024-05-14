154630f4778c88cb5aff16fd91f47e5f74ea923e
dwriz <dwikyrizkya27@gmail.com>
2024-05-14 20:47:56 +0700

    server | chore: setup middleware errorHandler()

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,30 @@
+function errorHandler(error, req, res, next) {
+    console.log(error); // delete before prod
+    switch (error.name) {
+      case "SequelizeValidationError":
+      case "SequelizeUniqueConstraintError":
+        res.status(400).json({ message: error.errors[0].message });
+        return;
+      case "EmailEmpty":
+        res.status(400).json({ message: "Validation error" });
+        return;
+      case "PassEmpty":
+        res.status(400).json({ message: "Validation error" });
+        return;
+      case "InvalidLogin":
+        res.status(401).json({ message: "Error authentication" });
+        return;
+      case "InvalidToken":
+      case "JsonWebTokenError":
+        res.status(401).json({ message: "Invalid token" });
+        return;
+      case "OwnerRestricted":
+        res.status(403).json({ message: "Restricted to owner" });
+        return;
+      default:
+        res.status(500).json({ message: "Internal server error" });
+        return;
+    }
+  }
+  
+  module.exports = { errorHandler };
