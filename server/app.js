d474c08eefce497e5ca7980b7833aa833c555822
dwriz <dwikyrizkya27@gmail.com>
2024-05-14 21:48:12 +0700

    server | chore: add dotenv, error handler, router to app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const { errorHandler } = require("./middlewares/errorHandler.js");
+
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -5,7 +9,9 @@ const port = 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(require("./routers"));
+app.use(require("./routers/index.js"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
