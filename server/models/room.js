37dd7a0be0bb7d992863d2494f0444928886d809
dwriz <dwikyrizkya27@gmail.com>
2024-05-14 22:03:06 +0700

    server | fix: add association and constraints to model Room

--- server/models/room.js
+++ server/models/room.js
@@ -1,30 +1,37 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Room extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Room.belongsTo(models.User, { foreignKey: "UserId" });
     }
   }
-  Room.init({
-    UserId: DataTypes.INTEGER,
-    prompt1: DataTypes.STRING,
-    prompt2: DataTypes.STRING,
-    prompt3: DataTypes.STRING,
-    finalPrompt: DataTypes.STRING,
-    answer: DataTypes.STRING,
-    imgUrl: DataTypes.STRING,
-    accuracyRate: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Room',
-  });
+  Room.init(
+    {
+      UserId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Email is required",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+        },
+      },
+      prompt1: DataTypes.STRING,
+      prompt2: DataTypes.STRING,
+      prompt3: DataTypes.STRING,
+      finalPrompt: DataTypes.STRING,
+      answer: DataTypes.STRING,
+      imgUrl: DataTypes.STRING,
+      accuracyRate: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Room",
+    }
+  );
   return Room;
 };
