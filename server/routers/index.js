b998279b06f1ba99cb56ce5c512614d7f2630323
dwriz <dwikyrizkya27@gmail.com>
2024-05-15 16:28:21 +0700

    server | feat: add authentication for API protection

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { authentication } = require("../middlewares/authentication.js");
 
 router.post("/", async (req, res) => {
   res.send("Hello! This is / route");
 });
 
 router.use(require("./auth.js"));
+
+router.use(authentication);
+
 router.use(require("./game.js"));
 
 module.exports = router;
