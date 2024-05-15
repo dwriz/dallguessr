e9b3489d2aeedb06d1a44e598cc67d047dbaa4ef
dwriz <dwikyrizkya27@gmail.com>
2024-05-15 16:30:08 +0700

    server | fix: add params id to add-room routing

--- server/routers/game.js
+++ server/routers/game.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { GameController } = require("../controllers/gameController.js");
 
-router.post("/add-room", GameController.createRoom);
+router.post("/user/:id/add-room", GameController.createRoom);
 
 module.exports = router;
