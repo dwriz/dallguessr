a541fa6dbd70067875f2bcd7c2aee81baa015986
dwriz <dwikyrizkya27@gmail.com>
2024-05-15 16:33:22 +0700

    server | feat: working data pipeline + integrate cloudinary to createRoom() feature

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,13 +1,38 @@
 const { createImagePrompts, createImageUrl } = require("../helpers/openai.js");
+const { cloudinary } = require("../helpers/cloudinary.js");
+const { Room } = require("../models/index.js");
 
 class GameController {
   static async createRoom(req, res, next) {
     try {
-      let prompts = await createImagePrompts();
+      const promptsRaw = await createImagePrompts();
+
+      console.log(promptsRaw);
+      console.log("======= raw prompts =======");
+
+      const prompts = JSON.parse(promptsRaw);
+      
       console.log(prompts);
-      let imageUrl = await createImageUrl(prompts[3]);
-      console.log(imageUrl);
-      res.send(imageUrl);
+      console.log("======= processed prompts =======");
+
+      const room = await Room.create({
+        UserId: req.params.id,
+        prompt1: prompts[0],
+        prompt2: prompts[1],
+        prompt3: prompts[2],
+        finalPrompt: prompts[3],
+      });
+
+      const imageUrl = await createImageUrl(prompts[3]);
+
+      const cloudinaryUrl = await cloudinary.uploader.upload(imageUrl, {
+        folder: "dallguessr",
+        public_id: room.id,
+      });
+
+      await room.update({ imgUrl: cloudinaryUrl.secure_url });
+
+      res.status(200).json(room);
     } catch (error) {
       next(error);
     }
