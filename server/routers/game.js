e63ef332899a28640aaed0559fe7f5b5fff7b5a9
dwriz <dwikyrizkya27@gmail.com>
2024-05-16 20:32:37 +0700

    server | fix: showDashboard() will handle data without needing url params

--- server/routers/game.js
+++ server/routers/game.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { GameController } = require("../controllers/gameController.js");
 
 router.post("/user/:UserId/add-room", GameController.createRoom);
-router.get("/user/:UserId/rooms", GameController.showDashboard);
+router.get("/dashboard", GameController.showDashboard);
 router.get("/user/:UserId/rooms/:RoomId", GameController.showRoom);
 router.put("/user/:UserId/rooms/:RoomId", GameController.postAnswer);
 router.delete("/user/:UserId/rooms/:RoomId", GameController.deleteRoom);
