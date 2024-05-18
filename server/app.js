6caf81ebe00f1fe1ba44c0b00bdf19b60d029567
dwriz <dwikyrizkya27@gmail.com>
2024-05-18 16:43:51 +0700

    fix: disable dotenv on prod env

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
 
 const { errorHandler } = require("./middlewares/errorHandler.js");
 const cors = require("cors");
